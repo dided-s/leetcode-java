9b912a2203816ba5bdb2e2bd0dc6d507995b4924
gilsar <konick.qlippoth@gmail.com>
2026-06-28 09:01:08 +0300

    Решение 151_Reverse_Words_in_a_String

---
+++ src/main/java/medium/_151_Reverse_Words_in_a_String/Solution.java
@@ -0,0 +1,35 @@
+package medium._151_Reverse_Words_in_a_String;
+
+import annotations.LeetCode75;
+import annotations.Medium;
+
+@Medium
+@LeetCode75("Array / String")
+public class Solution {
+
+    public String reverseWords(String s) {
+        if (s == null) throw new IllegalArgumentException();
+        if (s.length() < 2) return s;
+        s = " " + s + " ";
+
+        StringBuilder result = new StringBuilder();
+
+        int i = s.length() - 1;
+        int j = s.length() - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (s.charAt(i) != ' ') {
+                i--;
+            } else if (i == j) {
+                i--;
+                j--;
+            } else {
+                result.append(s.substring(i, j + 1));
+                i--;
+                j = i;
+            }
+        }
+
+        return result.deleteCharAt(0).toString();
+    }
+}
---
+++ src/main/java/medium/_151_Reverse_Words_in_a_String/Solution2.java
@@ -0,0 +1,25 @@
+package medium._151_Reverse_Words_in_a_String;
+
+import annotations.LeetCode75;
+import annotations.Medium;
+
+@Medium
+@LeetCode75("Array / String")
+public class Solution2 {
+
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\s+");
+        if (words.length == 0) {
+            throw new IllegalArgumentException("Empty string");
+        }
+        StringBuilder result = new StringBuilder();
+        result.append(words[words.length - 1]);
+
+        for (int i = words.length - 2; i >= 0; i--) {
+            result.append(' ');
+            result.append(words[i]);
+        }
+
+        return result.toString();
+    }
+}
